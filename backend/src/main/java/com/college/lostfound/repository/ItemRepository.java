1956c649cce5a1ae7a3c4a1b14717355eae229ad
Nitesh N D <niteshdwaraka@gmail.com>
2025-09-29 23:02:14 +0530

    Update ItemRepository.java

--- backend/src/main/java/com/college/lostfound/repository/ItemRepository.java
+++ backend/src/main/java/com/college/lostfound/repository/ItemRepository.java
@@ -2,6 +2,9 @@ package com.college.lostfound.repository;
 
 import com.college.lostfound.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByTitleContainingIgnoreCase(String q);
+    List<Item> findByCategory(String category);
 }
